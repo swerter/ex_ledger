82d913a90746015ac18bd9b84ec7b2405bf3e8ac
Michael J. Bruderer <mjb@migadu.ch>
2025-11-28 12:03:00 +0100

    Add mix.exs/lock

---
+++ mix.exs
@@ -0,0 +1,29 @@
+defmodule ExLedger.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :ex_ledger,
+      version: "0.1.0",
+      elixir: "~> 1.19",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:nimble_parsec, "~> 1.0"},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false}
+    ]
+  end
+end

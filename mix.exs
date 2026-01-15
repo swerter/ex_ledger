f972e8c7d47c18fb17f5d1dcae1730426cef6897
Michael J. Bruderer <mjb@migadu.ch>
2026-01-15 09:16:38 +0100

    Update version to 0.1.4

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExLedger.MixProject do
   def project do
     [
       app: :ex_ledger,
-      version: "0.1.3",
+      version: "0.1.4",
       elixir: "~> 1.19",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
